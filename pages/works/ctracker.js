008a2a64c1188f9860b24a31a11c62b2fa7cceee
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-08-30 19:50:31 +0530

    ctracker

--- pages/works/ctracker.js
+++ pages/works/ctracker.js
@@ -1,38 +1,28 @@
 import Header from '../components/project/Header';
-import CalendarTodoFillIcon from 'remixicon-react/CalendarTodoFillIcon';
 import Image from 'next/image';
 
 function ctracker() {
     return (
         <>
             <Header />
-            <main className="">
-                <section className=" pt-[145px]">
-                    <div className="flex flex-row ">
+            <main className="max-w-[1160px] mx-auto">
+                <section className="pt-[145px]">
+                    <div className="flex flex-row justify-between ">
                         <div className="">
-                            <div className="flex flex-row space-x-[50px] mx-[220px] px-8 sm:px-16">
-                                <CalendarTodoFillIcon className="text-ctracker1" />
-                                <span className="text-ctracker1 text-[16px] font-medium">June 2021</span>
-                            </div>
-                            <div className="pr-[780px]">
-                                <h2 className="text-[48px] font-extrabold text-ctracker3 pb-[16px] mx-[220px] px-8 sm:px-16">Ctracker - Track your foot and confirm your footprint</h2>
-                            </div>
-                            {/* <div className="bg-ctracker2  h-[398px] w-full"> */}
-                            <div className="h-[532px]">
-                                <p className="font-normal text-[20px] text-ctracker3 mx-[220px] px-8 pt-[8px] sm:px-16">
-                                    Maintaining records of staff, customers and
-                                    visitors for<br /> contact tracing purposes, as set out in the
-                                    Government<br /> guidance due to COVID-19.
-                                </p>
-                                <ul className="list-disc font-normal text-[20px] pt-[16px] mx-[260px] text-ctracker3 max-w-[90rem] px-8 sm:px-16">
-                                    <li>Flutter QR Code Scanner Application
-                                        <li>Dark and Light Modes</li>
-                                        <li>Received <span className="text-ctracker1">237 Duplicates</span> and <span className="text-ctracker1">7 Favourites</span> in Figma <br />Community</li></li>
-                                </ul>
-                            </div>
-                            {/* </div> */}
+                            <span className="text-ctracker1 text-[16px] font-medium">June 2021</span>
+                            <h2 className="text-[48px] font-extrabold text-ctracker3 pb-[16px] ">Ctracker - Track your foot and confirm your footprint</h2>
+                            <p className="font-normal text-[20px] text-ctracker3 pt-[8px]">
+                                Maintaining records of staff, customers and
+                                visitors for contact tracing purposes, as set out in the
+                                Government guidance due to COVID-19.
+                            </p>
+                            <ul className="list-disc font-normal text-[20px] pt-[16px] mx-[40px] text-ctracker3">
+                                <li>Flutter QR Code Scanner Application</li>
+                                <li>Dark and Light Modes</li>
+                                <li>Received <span className="text-ctracker1">237 Duplicates</span> and <span className="text-ctracker1">7 Favourites</span> in Figma Community</li>
+                            </ul>
                         </div>
-                        <div className="absolute h-[507px] w-[360px] right-[350px] pt-[150px]">
+                        <div className="relative h-auto w-[380px] ">
                             <Image
                                 src='/Portfolio/Ctracker-New.png'
                                 layout="fill"
@@ -43,8 +33,8 @@ function ctracker() {
                     </div>
                 </section>
                 <section className="">
-                    <h1 className="font-semibold text-[32px] text-ctracker1 pt-[140px] mx-[220px] px-8 sm:px-16">Style Guides</h1>
-                    <div className="absolute h-[393px] w-[1160px] mt-[24px] mx-[300px] px-8 sm:px-16">
+                    <h1 className="font-semibold text-[32px] text-ctracker1 pt-[140px]">Style Guides</h1>
+                    <div className="absolute h-[393px] w-[1160px] mt-[24px]">
                         <Image
                             src='/Portfolio/Ctracker-Style-Guides.png'
                             layout="fill"
@@ -52,8 +42,8 @@ function ctracker() {
                             className=""
                         />
                     </div>
-                    <h1 className="font-semibold text-[32px] text-ctracker1 pt-[500px] mx-[220px] px-8 sm:px-16">Userflow Diagram</h1>
-                    <div className="absolute h-[412px] w-[1160px] mt-[24px] mx-[300px] px-8 sm:px-16">
+                    <h1 className="font-semibold text-[32px] text-ctracker1 pt-[500px]">Userflow Diagram</h1>
+                    <div className="absolute h-[412px] w-[1160px] mt-[24px]">
                         <Image
                             src='/Portfolio/Ctracker-Userflow-Diagram.png'
                             layout="fill"
@@ -62,8 +52,8 @@ function ctracker() {
                         />
                     </div>
 
-                    <h1 className="font-semibold text-[32px] text-ctracker1 pt-[500px] mx-[220px] px-8 sm:px-16">Wireframes</h1>
-                    <div className="absolute h-[367px] w-[1160px] mt-[24px] mx-[300px] px-8 sm:px-16">
+                    <h1 className="font-semibold text-[32px] text-ctracker1 pt-[500px]">Wireframes</h1>
+                    <div className="absolute h-[367px] w-[1160px] mt-[24px]">
                         <Image
                             src='/Portfolio/Ctracker-Wireframes.png'
                             layout="fill"
@@ -71,8 +61,8 @@ function ctracker() {
                             className=""
                         />
                     </div>
-                    <h1 className="font-semibold text-[32px] text-ctracker1 pt-[500px]  mx-[220px] px-8 sm:px-16">Final UI</h1>
-                    <div className="relative h-[829px] w-[1236px] mt-[24px]  mx-[300px] px-8 sm:px-16">
+                    <h1 className="font-semibold text-[32px] text-ctracker1 pt-[500px]">Final UI</h1>
+                    <div className="relative h-[829px] w-[1236px] mt-[24px]">
                         <Image
                             src='/Portfolio/Ctracker-Final-UI-1.png'
                             layout="fill"
@@ -80,7 +70,7 @@ function ctracker() {
                             className=""
                         />
                     </div>
-                    <div className="relative h-[829px] w-[1236px] mt-[24px]  mx-[300px] px-8 sm:px-16">
+                    <div className="relative h-[829px] w-[1236px] mt-[24px]">
                         <Image
                             src='/Portfolio/Ctracker-Final-UI-2.png'
                             layout="fill"
@@ -90,8 +80,8 @@ function ctracker() {
                     </div>
                 </section>
                 <section >
-                    <h1 className="font-extrabold text-center text-[36px] text-accent my-[140px] mx-[220px] px-8 sm:px-16">Other Featured Projects</h1>
-                    <div className=" inline-flex flex-wrap space-x-[22.47px] mx-[300px] px-8 sm:px-16">
+                    <h1 className="font-extrabold text-center text-[36px] text-accent my-[140px]">Other Featured Projects</h1>
+                    <div className=" inline-flex flex-wrap space-x-[22.47px]">
                         <div className="relative h-[371.69px] w-[371.69px] mt-[24px] ">
                             <Image
                                 src='/Image-Assets/Ctracker.png'
