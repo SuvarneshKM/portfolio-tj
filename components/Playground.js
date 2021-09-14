bb956a3e046db1b07f632493d0a0b73f36c48e6d
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-09-14 15:02:42 +0530

    playground

--- components/Playground.js
+++ components/Playground.js
@@ -2,16 +2,16 @@ import Image from 'next/image';
 
 function Playground() {
     return (
-        <section className="lg:pt-[80px] px-[16px] py-[16px]">
-            <h2 className="text-[36px] text-left lg:text-center font-extrabold text-accent">
-                Playground
-            </h2>
-            <div className="grid grid-cols-2
-                lg:grid-cols-3 xl:grid-cols-4 ">
-                <div className="flex items-center m-2 mt-5 space-x-4 rounded-lg cursor-pointer
-        hover:bg-gray-100 hover:scale-105 transition
-        transform duration-200 ease-out">
-                    <div className="relative h-[135px] w-[135px] md:h-[270px] lg:p-20 md:w-[270px] ">
+        <>
+            <section className="lg:pt-[200px] pt-[100px]" >
+                <h2 className="text-[36px] text-left lg:text-center font-extrabold text-accent">
+                    Playground
+                </h2>
+                <div className="lg:flex flex-wrap lg:flex-wrap lg:pt-[80px] pt-[40px]">
+                    <div className="
+                            items-center m-2 mt-5 space-x-4 rounded-lg cursor-pointer
+                             hover:scale-105 transition
+                            transform duration-200 ease-out relative lg:h-[360px] lg:w-[360px] flex-shrink lg:flex-shrink-0">
                         <Image
                             src='/Playground/Cart.png'
                             layout="fill"
@@ -19,11 +19,21 @@ function Playground() {
                             className=""
                         />
                     </div>
-                </div>
-                <div className="flex items-center m-2 mt-5 space-x-4 rounded-lg cursor-pointer
-        hover:bg-gray-100 hover:scale-105 transition
-        transform duration-200 ease-out">
-                    <div className="relative h-[135px] w-[135px] md:h-[270px] lg:p-20 md:w-[270px] ">
+                    <div className="
+                            items-center m-2 mt-5 space-x-4 rounded-lg cursor-pointer
+                             hover:scale-105 transition
+                            transform duration-200 ease-out relative aspect-h-1 aspect-w-1 lg:hidden flex-shrink lg:flex-shrink-0">
+                        <Image
+                            src='/Playground/Cart.png'
+                            layout="fill"
+                            objectFit="cover"
+                            className=""
+                        />
+                    </div>
+                    <div className="
+                            items-center m-2 mt-5 space-x-4 rounded-lg cursor-pointer
+                             hover:scale-105 transition
+                            transform duration-200 ease-out relative lg:h-[360px] lg:w-[360px] flex-shrink lg:flex-shrink-0">
                         <Image
                             src='/Playground/Covid.png'
                             layout="fill"
@@ -31,11 +41,21 @@ function Playground() {
                             className=""
                         />
                     </div>
-                </div>
-                <div className="flex items-center m-2 mt-5 space-x-3 rounded-lg cursor-pointer
-        hover:bg-gray-100 hover:scale-105 transition
-        transform duration-200 ease-out">
-                    <div className="relative h-[135px] w-[135px] md:h-[270px] lg:p-20 md:w-[270px] ">
+                    <div className="
+                            items-center m-2 mt-5 space-x-4 rounded-lg cursor-pointer
+                             hover:scale-105 transition
+                            transform duration-200 ease-out relative aspect-h-1 aspect-w-1 lg:hidden flex-shrink lg:flex-shrink-0">
+                        <Image
+                            src='/Playground/Covid.png'
+                            layout="fill"
+                            objectFit="cover"
+                            className=""
+                        />
+                    </div>
+                    <div className="
+                            items-center m-2 mt-5 space-x-4 rounded-lg cursor-pointer
+                             hover:scale-105 transition
+                            transform duration-200 ease-out relative lg:h-[360px] lg:w-[360px] flex-shrink lg:flex-shrink-0">
                         <Image
                             src='/Playground/Login.png'
                             layout="fill"
@@ -43,11 +63,21 @@ function Playground() {
                             className=""
                         />
                     </div>
-                </div>
-                <div className="flex items-center m-2 mt-5 space-x-3 rounded-lg cursor-pointer
-        hover:bg-gray-100 hover:scale-105 transition
-        transform duration-200 ease-out">
-                    <div className="relative h-[135px] w-[135px] md:h-[270px] lg:p-20 md:w-[270px] ">
+                    <div className="
+                            items-center m-2 mt-5 space-x-4 rounded-lg cursor-pointer
+                             hover:scale-105 transition
+                            transform duration-200 ease-out relative aspect-h-1 aspect-w-1 lg:hidden flex-shrink lg:flex-shrink-0">
+                        <Image
+                            src='/Playground/Login.png'
+                            layout="fill"
+                            objectFit="cover"
+                            className=""
+                        />
+                    </div>
+                    <div className="
+                            items-center m-2 mt-5 space-x-4 rounded-lg cursor-pointer
+                             hover:scale-105 transition
+                            transform duration-200 ease-out relative lg:h-[360px] lg:w-[360px] flex-shrink lg:flex-shrink-0">
                         <Image
                             src='/Playground/Portfolio.png'
                             layout="fill"
@@ -55,11 +85,32 @@ function Playground() {
                             className=""
                         />
                     </div>
-                </div>
-                <div className="flex items-center m-2 mt-5 space-x-3 rounded-lg cursor-pointer
-        hover:bg-gray-100 hover:scale-105 transition
-        transform duration-200 ease-out">
-                    <div className="relative h-[135px] w-[135px] md:h-[270px] lg:p-20 md:w-[270px] ">
+                    <div className="
+                            items-center m-2 mt-5 space-x-4 rounded-lg cursor-pointer
+                             hover:scale-105 transition
+                            transform duration-200 ease-out relative aspect-h-1 aspect-w-1 lg:hidden flex-shrink lg:flex-shrink-0">
+                        <Image
+                            src='/Playground/Portfolio.png'
+                            layout="fill"
+                            objectFit="cover"
+                            className=""
+                        />
+                    </div>
+                    <div className="
+                            items-center m-2 mt-5 space-x-4 rounded-lg cursor-pointer
+                             hover:scale-105 transition
+                            transform duration-200 ease-out relative lg:h-[360px] lg:w-[360px] flex-shrink lg:flex-shrink-0">
+                        <Image
+                            src='/Playground/Resume-Template.png'
+                            layout="fill"
+                            objectFit="cover"
+                            className=""
+                        />
+                    </div>
+                    <div className="
+                            items-center m-2 mt-5 space-x-4 rounded-lg cursor-pointer
+                             hover:scale-105 transition
+                            transform duration-200 ease-out relative aspect-h-1 aspect-w-1 lg:hidden flex-shrink lg:flex-shrink-0">
                         <Image
                             src='/Playground/Resume-Template.png'
                             layout="fill"
@@ -68,8 +119,8 @@ function Playground() {
                         />
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 }
 
